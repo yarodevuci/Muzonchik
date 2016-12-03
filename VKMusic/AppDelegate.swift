7e941b7d16736034af3432d0d0867d6d99afe5d4
Yaro <yaro@koder.ai>
2016-12-03 01:38:41 -0800

    + Code refactor and review

--- VKMusic/AppDelegate.swift
+++ VKMusic/AppDelegate.swift
@@ -19,25 +19,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         vkDelegateReference = VKDelegateImpl()
-        //_ = VKDelegateImpl(window_: window!)
         return true
     }
     
     func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
         backgroundSessionCompletionHandler = completionHandler
     }
-    //iOS 8 and lower
+
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         VK.process(url: url, sourceApplication: sourceApplication)
         return true
     }
-    //iOS 9 and higher ..
-//    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-//        if #available(iOS 9.0, *) {
-//            VK.process(url: url, sourceApplication: options)
-//        } 
-//        return true
-//    }
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
