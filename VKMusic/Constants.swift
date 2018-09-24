dc65eeb7833e1a65e2567872f4cfa9664a6e5c5c
Yaro <dukalyar@gmail.com>
2018-09-23 22:58:33 -0700

    - updated constants

--- VKMusic/Constants.swift
+++ VKMusic/Constants.swift
@@ -14,10 +14,10 @@ let SEARCH_URL = WEB_BASE_URL + "music/search/"
 let ONE_SIGNAL_APP_ID = "d9d4d060-a3b8-4324-9474-eafea38ee267"
 
 //Local APIS URL
-let SERVER_RMQ_URL = "amqp://yaroslav:dukalis@50.18.38.224"
-let RMQConnection_URI = "amqp://yaroslav:dukalis@192.168.29.175"
+let SERVER_RMQ_URL = "amqp://yaroslav:dukalis@192.168.0.6"
+let RMQConnection_URI = "amqp://yaroslav:dukalis@192.168.0.6"
 
-let LOCAL_API_SERVER_ADDRESS = "http://192.168.29.175:8080/"
+let LOCAL_API_SERVER_ADDRESS = "http://192.168.0.6:8080/"
 let SERVER_API_ADDRESS = LOCAL_API_SERVER_ADDRESS//"http://50.18.38.224/"
 
 let YOUTUBE_CONVERTER_API = URL(string: SERVER_API_ADDRESS + "youtube_url_to_audio")!
